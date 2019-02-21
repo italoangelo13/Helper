e9aab0f06da5194a7233fc9be31b53abc87e103e
italoangelo13 <italoangelo.ti@gmail.com>
2019-02-21 17:47:35 -0300

    dashboard

--- Helper/Services/Versao.asmx.cs
+++ Helper/Services/Versao.asmx.cs
@@ -1,6 +1,7 @@
 ﻿using Newtonsoft.Json;
 using System;
 using System.Collections.Generic;
+using System.Data;
 using System.Linq;
 using System.Web;
 using System.Web.Script.Services;
@@ -25,6 +26,33 @@ namespace Helper
         {
             string versao = System.Reflection.Assembly.GetExecutingAssembly().GetName().Version.ToString();
             return JsonConvert.SerializeObject(versao);
+        }
+
+
+        [WebMethod]
+        public string atualizaDashboard(int CodUsuario, string Usuario)
+        {
+            if (Usuario.ToUpper().Equals("ADMIN"))
+            {
+                int ticketAberto = 0;
+                BancoDados b = new BancoDados();
+                //Ticket's em aberto
+                b.Query(@"SELECT Count(*) AS ticket 
+                            FROM   atendimentos 
+                            WHERE  ate_codstatus = 1 ");
+                DataTable dt = b.ExecutarDataTable();
+                if (dt.Rows.Count > 0)
+                {
+                    ticketAberto = int.Parse(dt.Rows[0]["ticket"].ToString());
+                }
+
+                return JsonConvert.SerializeObject(new { QTAbertos = ticketAberto});
+            }
+            else
+            {
+                return JsonConvert.SerializeObject(new { QTAbertos = 0 });
+            }
+            
         }
     }
 }
